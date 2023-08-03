4c2e618a51c6877508001dc01d66d8ad722ad7f8
cbattenplowright <callumbp15@gmail.com>
2023-08-03 13:59:33 +0100

    changed Get request to Post request following the RESTful routes

--- src/main/java/com/bnta/word_guesser/controllers/GameController.java
+++ src/main/java/com/bnta/word_guesser/controllers/GameController.java
@@ -2,18 +2,24 @@ package com.bnta.word_guesser.controllers;
 
 import com.bnta.word_guesser.models.Game;
 import com.bnta.word_guesser.models.Reply;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // Adding the @RestController annotation gives the controller the configuration needed to process the requests and serialisation of JSON files
 @RestController
+@RequestMapping(value = "/games") // This annotation signifies a generic prefix
 public class GameController {
 
-    // This is the method that will fire when the Get request is heard
-    @GetMapping("/games") // "/games" is the route that will trigger the GET request
-    public Reply newGame() {
+    // This is the method that will fire when the Post request is heard
+    @PostMapping
+    public ResponseEntity<Reply> newGame() {
         Game game = new Game("hello"); // Creates game object
-        return new Reply("*****", "New game started"); // Spring will serialise the return object and send to the client
+        Reply reply = new Reply("*****", "New game started");
+        return new ResponseEntity<>(reply, HttpStatus.OK); // When returning a ResponseEntity type requires an object and status code
     }
 
 }
